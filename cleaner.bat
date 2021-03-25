095a86cef69abf7644d566d383a3bbaf6d8a08d9
Ahmed Shahriar <47810596+shahriar0247@users.noreply.github.com>
2021-03-26 03:23:42 +0600

    Update cleaner.bat

--- cleaner.bat
+++ cleaner.bat
@@ -4,7 +4,7 @@ echo " ------Cleaner------ "
 echo " ------------------- "
 cd "C:\Windows\Temp"
 rd /s /q .
-cd "C:\Users\%username%\AppData\Local\Temp"
+cd %temp%
 rd /s /q .
 cd "C:\Windows\Prefetch"
 rd /s /q .
